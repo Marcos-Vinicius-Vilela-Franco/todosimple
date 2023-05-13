009dff02de6a85549d592a0850c60d4cea6e2085
Marcos-Vinicius-Vilela-Franco <mvmarcos74@gmail.com>
2023-05-13 09:21:42 -0300

    update 2

--- src/main/java/com/marcos/demo/models/User.java
+++ src/main/java/com/marcos/demo/models/User.java
@@ -1,11 +1,15 @@
 package com.marcos.demo.models;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -33,4 +37,7 @@ public class User {
     @NotEmpty
     @Size(min=8, max=60) 
     private String password;
+    
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 }
